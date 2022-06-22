819f5adf9fc2183aa2702aa8b09aa0136f59ba5d
JaumDarkz <viniciuspkfr@gmail.com>
2022-06-22 19:35:16 -0300

    feat: add dependency leves result

--- components/Dashboard/components/ResultLayout/index.tsx
+++ components/Dashboard/components/ResultLayout/index.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+
 import styles from './styles.module.scss'
 
-const ResultLayout = () => {
+const ResultLayout = (props: any) => {
   return (
-    <div>
-      
+    <div className={styles.screenContainer}>
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          Análise
+        </div>
+        <div className={styles.dependencyContainer}>
+          <Image src={props.dependency} alt='dependency' width={250}/>
+        </div>
+        <div className={styles.textLevel}>
+          {props.textLevel}
+        </div>
+        <div className={styles.nextButtonContainer}>
+          <div className={styles.nextButton}>
+            Seguinte
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -4,6 +4,10 @@ import FarForm from "../FagerstromForm";
 import SusForm from "../SusForm";
 import ResultLayout from "./components/ResultLayout";
 
+import normal from './components/ResultLayout/assets/normal.svg'
+import medium from './components/ResultLayout/assets/medium.svg'
+import high from './components/ResultLayout/assets/high.svg'
+
 import styles from "./styles.module.scss";
 import { useState } from "react";
 
@@ -15,6 +19,9 @@ const Dash = () => {
       <Sidebar image={"/images/avatar.jpg"} page={page} setPage={setPage} />
       <SusForm page={page} />
       <FarForm page={page} />
+      <ResultLayout dependency={normal} textLevel='BAIXA' />
+      <ResultLayout dependency={medium} textLevel='MÉDIA' />
+      <ResultLayout dependency={high} textLevel='ALTA' />
     </div>
   );
 };
