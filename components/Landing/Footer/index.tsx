4a837362c8a5cd6f5b7be640025fb9755f3e330d
JaumDarkz <viniciuspkfr@gmail.com>
2022-07-04 15:44:19 -0300

    feat: add finish mobile responsive

--- components/Landing/Footer/index.tsx
+++ components/Landing/Footer/index.tsx
@@ -25,42 +25,6 @@ const Footer = () => {
             <div>Busque Especialistas</div>
           </div>
         </div>
-
-        <div className={styles.column}>
-          <div className={styles.title}>
-            Home
-          </div>
-          <div className={styles.content}>
-            <div>Para Pacientes</div>
-            <div>Para Especialistas</div>
-            <div>Para Empresas</div>
-            <div>Busque Especialistas</div>
-          </div>
-        </div>
-
-        <div className={styles.column}>
-          <div className={styles.title}>
-            Home
-          </div>
-          <div className={styles.content}>
-            <div>Para Pacientes</div>
-            <div>Para Especialistas</div>
-            <div>Para Empresas</div>
-            <div>Busque Especialistas</div>
-          </div>
-        </div>
-
-        <div className={styles.column}>
-          <div className={styles.title}>
-            Home
-          </div>
-          <div className={styles.content}>
-            <div>Para Pacientes</div>
-            <div>Para Especialistas</div>
-            <div>Para Empresas</div>
-            <div>Busque Especialistas</div>
-          </div>
-        </div>
       </div>
       <div className={styles.line}/>
       <div className={styles.footerEnd}>
@@ -68,9 +32,6 @@ const Footer = () => {
           SanoPlace - CNPJ: 00.000.000/0000-00 - em breve<br />
           <br />
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an <br />
-          unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic <br />
-          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently <br />
-          with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </div>
         <div className={styles.info}>
           <div>@2022 SanoPlace</div>
